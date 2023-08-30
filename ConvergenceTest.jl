b0c22766c482f8e879a99f9aa33981d4ffeb8a10
Steven Brus <steven.r.brus@gmail.com>
2023-08-30 11:38:36 -0700

    Separate out driver function

--- ConvergenceTest.jl
+++ ConvergenceTest.jl
@@ -1,92 +1,11 @@
-#   Coastal Kelvin Wave Test Case
-#   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-
-#   simulating a coastal kelvin wave
-
 CODE_ROOT = pwd() * "/"
 
-include(CODE_ROOT * "mode_init/MPAS_Ocean.jl")
-include(CODE_ROOT * "mode_forward/time_steppers.jl")
+include(CODE_ROOT * "mode_forward/driver.jl")
 include(CODE_ROOT * "visualization.jl")
-include(CODE_ROOT * "mode_init/exactsolutions.jl")
-
-using PyPlot
-using PyCall
-
-animation  = pyimport("matplotlib.animation")
-ipydisplay = pyimport("IPython.display")
 
 using LinearAlgebra # for norm()
 import Dates
 using DelimitedFiles
-
-
-
-function model_run(test_case, mesh_directory, mesh_file_name, periodicity, T, output_path, nSaves=1;
-        plot=false, animate=false, nvlevels=1)
-
-    mpasOcean = MPAS_Ocean(mesh_directory, mesh_file_name, periodicity=periodicity, nvlevels=nvlevels)
-    
-    meanCoriolisParameterf = sum(mpasOcean.fEdge) / length(mpasOcean.fEdge)
-    meanFluidThicknessH = sum(mpasOcean.bottomDepth)/length(mpasOcean.bottomDepth)
-    c = sqrt(mpasOcean.gravity*meanFluidThicknessH)
-    
-    println("simulating for T: $T")
-    lYedge = maximum(mpasOcean.yEdge) - minimum(mpasOcean.yEdge)
-
-    println("generating exact methods for mesh")
-    if test_case == "kelvinWave"
-        exactNormalVelocity, exactSSH, exactSolution!, boundaryCondition! = kelvinWaveGenerator(mpasOcean)
-    elseif test_case == "inertiaGravityWave"
-        exactNormalVelocity, exactSSH, exactSolution! = inertiaGravityWaveGenerator(mpasOcean)
-    end
-    
-    println("setting up initial condition")
-    exactSolution!(mpasOcean)
-    mpasOcean.layerThicknessOld = copy(mpasOcean.layerThickness)
-    mpasOcean.normalVelocityOld = copy(mpasOcean.normalVelocity)
-    mpasOcean.layerThicknessNew = copy(mpasOcean.layerThickness)
-    mpasOcean.normalVelocityNew = copy(mpasOcean.normalVelocity)
-    
-    sshExact = zeros(Float64, (mpasOcean.nCells))
-    
-    if plot
-        calculate_ssh_new!(mpasOcean)
-        sshExact = exactSSH(mpasOcean, 1:mpasOcean.nCells)
-        plotSSHs(1, mpasOcean, sshExact, "Initial Condition", output_path)
-    end
-    
-    println("original dt $(mpasOcean.dt)")
-    nSteps = Int(round(T/mpasOcean.dt/nSaves))
-    mpasOcean.dt = T / nSteps / nSaves
-    
-    println("dx $(mpasOcean.dcEdge[1]) \t dt $(mpasOcean.dt) \t dx/c $(maximum(mpasOcean.dcEdge) / c) \t dx/dt $(mpasOcean.dcEdge[1]/mpasOcean.dt)")
-    
-    t = 0
-    for i in 1:nSaves
-        for j in 1:nSteps
-             
-            forward_rk4!(mpasOcean,  t)
-            #forward_backward_step!(mpasOcean)
-
-            t += mpasOcean.dt
-
-        end
-        println("t: $t")
-        if plot
-            calculate_ssh_new!(mpasOcean)
-            sshExact = exactSSH(mpasOcean, 1:mpasOcean.nCells, t)
-            plotSSHs(i+1, mpasOcean, sshExact, "T = $(t)", output_path)
-        end
-    end
-
-    sshExact = exactSSH(mpasOcean, 1:mpasOcean.nCells, t) 
-    error = mpasOcean.ssh .- sshExact
-    MaxErrorNorm = norm(error, Inf)
-    L2ErrorNorm = norm(error/sqrt(float(mpasOcean.nCells)))
-    
-    return mpasOcean.nCells, mpasOcean.dt, MaxErrorNorm, L2ErrorNorm
-end
 
 
 function convergence_test(test_case;
@@ -139,14 +58,14 @@ function convergence_test(test_case;
     end
 end
 
-convergence_test(
-            "kelvinWave",
-            resolutions=[32, 64, 144, 216, 324],
-            format=(x->"$(x)x$(x)"),
-            write_data=true, show_plots=true, nvlevels=1)
-
 #convergence_test(
-#            "inertiaGravityWave",
-#            resolutions=[64, 144, 216, 324],
+#            "kelvinWave",
+#            resolutions=[32, 64, 144, 216, 324],
 #            format=(x->"$(x)x$(x)"),
 #            write_data=true, show_plots=true, nvlevels=1)
+
+convergence_test(
+            "inertiaGravityWave",
+            resolutions=[64, 144, 216, 324],
+            format=(x->"$(x)x$(x)"),
+            write_data=true, show_plots=true, nvlevels=1)
