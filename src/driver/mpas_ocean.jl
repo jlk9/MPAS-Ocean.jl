1e2461e6939ce31115f59965dc7980a13ed3120b
Andrew Nolan <anolan@lanl.gov>
2023-12-05 15:08:32 -0800

    Begin building out Omega style TimeManager.
        Now that DateTime/Periods are parsed from the YAML file,
        work on creating the neccessary TimeManager structures as detailed
        in the Omega design documents.

--- src/driver/mpas_ocean.jl
+++ src/driver/mpas_ocean.jl
@@ -2,17 +2,47 @@
 #        julia package. 
 
 include("../infra/Config.jl") 
+include("../infra/TimeManager.jl")
 
 Config = ConfigRead("/global/homes/a/anolan/MPAS-Ocean.jl/inertial_gravity_wave_100km.yml")
 
-time_managementConfig = ConfigGet(Config.namelist, "time_management")
 
-startTime = ConfigGet(time_managementConfig, "config_start_time")
-runDuration = ConfigGet(time_managementConfig, "config_run_duration")
+function ocn_forward_mode_setup_clock(Config::GlobalConfig)
 
-time_integrationConfig = ConfigGet(Config.namelist, "time_integration")
-dt = ConfigGet(time_integrationConfig, "config_dt")
+    # Get the nested Config objects 
+    time_managementConfig = ConfigGet(Config.namelist, "time_management")
+    time_integrationConfig = ConfigGet(Config.namelist, "time_integration")
+    
+    dt = ConfigGet(time_integrationConfig, "config_dt")
+    stop_time = ConfigGet(time_managementConfig, "config_stop_time")
+    start_time = ConfigGet(time_managementConfig, "config_start_time")
+    run_duration = ConfigGet(time_managementConfig, "config_run_duration")
+    restart_timestamp_name = ConfigGet(time_managementConfig, "config_restart_timestamp_name")
+
+    ## I think I need some example of this, becuase not immediately obvious to me how to 
+    ## deal with this. Or really, what this actually would look like. 
+    #if restart_timestamp_name == "file"
+    #
+    #else 
+    #  
+    #end 
+    
+    # both config_run_duration and config_stop_time specified. Ensure that values are consitent
 
 
+    if run_duration != "none" 
+        clock = mpas_create_clock(dt, start_time; runDuration=run_duration)
+        if stop_time != "none" 
+            start_time + run_duration != stop_time && println("Warning: config_run_duration and config_stop_time are inconsitent: using config_run_duration.")
+        end 
+    elseif stop_time != "none"
+        clock = mpas_create_clock(dt, start_time; stopTime=stop_time)
+    else 
+        throw("Error: Neither config_run_duration nor config_stop_time were specified.")
+    end
 
+    return clock
+end 
+
+clock = ocn_forward_mode_setup_clock(Config)
 
---
+++ src/infra/TimeManager.jl
@@ -0,0 +1,40 @@
+# creat a ESMF clock like structure 
+mutable struct Clock 
+
+    startTime::DateTime
+    currTime::DateTime
+    prevTime::Union{DateTime, Nothing}
+    nextTime::DateTime
+    timeStep::Period
+
+    # How does it know the stop time? Is that set as an alarm? 
+    function Clock(startTime::DateTime, timeStep::Period)
+        currTime = startTime 
+        prevTime = nothing # at the start of the sim. there has been no prev. time 
+        nextTime = currTime + timeStep
+        
+        # return an instance of our structure 
+        return new(startTime, currTime, prevTime, nextTime, timeStep)
+    end 
+end 
+
+
+
+
+function mpas_create_clock(timeStep, startTime; stopTime=nothing, runDuration=nothing)
+    
+    if !isnothing(runDuration)
+        stop_time = startTime + runDuration
+        if !isnothing(stopTime)
+            stopTime != stop_time || throw("stopTime and runDuration are inconsistent") 
+        end 
+    elseif !isnothing(stopTime)
+        stop_time = stopTime
+    else
+        throw(" neither stopTime nor runDuration are specified")
+    end 
+        
+    clock = Clock(startTime, timeStep)
+
+    return clock 
+end 
