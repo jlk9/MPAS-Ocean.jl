a96040d1a8b564eec36b2318ede3ddb31a67b38d
Andrew Nolan <anolan@lanl.gov>
2023-12-07 08:46:51 -0700

    Add Alarm structures to the TimeManager infrastrucutre.
        Create `OneTimeAlarm` and `PeriodicAlarm` which has most methods
        from `AbstractAlarm`. Still need to create a common constructor
        which paramertically creates one of the two alarm types based
        on the types of keyword arguments passed.

--- src/infra/TimeManager.jl
+++ src/infra/TimeManager.jl
@@ -1,5 +1,7 @@
+using Dates 
+
 # abstract alarm type 
-abstract type abstract_alarm end
+abstract type AbstractAlarm end 
 
 # creat a ESMF clock like structure 
 mutable struct Clock 
@@ -27,6 +29,80 @@ function advance!(clock::Clock)
     clock.nextTime = clock.currTime + clock.timeStep
 end 
 
+
+mutable struct OneTimeAlarm{S,B,DT} <: AbstractAlarm
+    name::S       # name of the alarm 
+
+    ringing::B      # alarm is currently ringing
+    stopped::B      # alarm had been stopped and not reset
+
+    ringTime::DT # time at/after which alarm rings 
+
+    function OneTimeAlarm(name::String, alarmTime::DateTime)
+        return new{String, Bool, DateTime}(name, false, false, alarmTime)
+    end 
+end 
+
+
+mutable struct PeriodicAlarm{S,B,DT,P} <: AbstractAlarm 
+    name::S                           # name of the alarm 
+
+    ringing::B                         # alarm is currently ringing
+    stopped::B                          # alarm had been stopped and not reset
+
+    ringTime::DT                     # time at/after which alarm rings 
+    ringInterval::P                   # interval at which this alarm rings
+    ringTimePrev::Union{Nothing, DT} # previous alaram time for periodic alarms
+
+    function PeriodicAlarm(name::String, alarmInterval::Period, intervalStart::DateTime)
+        # NOTE: should alarm ring on interval start? or only after the first interval 
+        #       could be optional keyword in construtor based on option from the `io` 
+        #       section of the namelist 
+        ringTime = intervalStart + alarmInterval 
+        
+        return new{String, Bool, DateTime, Period}(name, false, false, ringTime, alarmInterval, nothing)
+    end
+end
+
+
+# Methods that work for all types of alarms 
+function isRinging(alarm::AbstractAlarm)
+    return alarm.ringing
+end
+
+function updateStatus!(alarm::AbstractAlarm, currentTime::DateTime)
+    alarm.ringTime == currentTime && (alarm.ringing = true)
+end 
+
+function rename!(alarm::AbstractAlarm, newName::String)
+    alarm.name = newName
+end
+
+function stop!(alarm::AbstractAlarm)
+    alarm.ringing = false 
+end
+
+
+# Methods that have type specific behavior 
+function reset!(alarm::OneTimeAlarm; inTime::Union{Nothing, DateTime}=nothing)
+    stop!(alarm)
+
+    if !isnothing(inTime)
+        alarm.ringTime = inTime
+    else 
+        alarm.stopped = true 
+    end 
+end
+
+# NOTE: Should reset method for PeriodicAlarms accepct a new ringTime?? 
+function reset!(alarm::PeriodicAlarm)
+    stop!(alarm)
+
+    # store the ringtime just turned off as the pervious ringtime
+    alarm.ringTimePrev = alarm.ringTime
+    # set the next ringTime
+    alarm.ringTime = alarm.ringTimePrev + alarm.ringInterval
+end 
 
 function mpas_create_clock(timeStep, startTime; stopTime=nothing, runDuration=nothing)
     
