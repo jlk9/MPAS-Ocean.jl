36eaa6a173f7c4a94d5b696d26895438c1780d79
Andrew Nolan <anolan@lanl.gov>
2024-02-06 07:40:07 -0800

    Build out top levels loops for timestepping and running the model.
        - Starting with Forward Euler timesteping, I've started building
          out a driver script capable of parsing a YAML config file
          generated by Polaris and run the model forward.
        - More timesteppers to come, which should be dispatched based off
          of type. Driver needs to be cleaned up and generalized, with
          a command line executable to be used by polaris.

--- src/driver/mpas_ocean.jl
+++ src/driver/mpas_ocean.jl
@@ -1,7 +1,9 @@
+using Dates
 using MPAS_O
-
+using NCDatasets
 
 # file path to the config file. Should be parsed from the command line 
+#config_fp = "../../TestData/inertial_gravity_wave_100km.yml"
 config_fp = "/global/homes/a/anolan/MPAS-Ocean.jl/bare_minimum.yml"
 
 # Initialize the Model  
@@ -9,13 +11,70 @@ Setup, Diag, Tend, Prog = ocn_init(config_fp)
 
 clock = Setup.timeManager
 
+Setup.timeManager.timeStep /= 5
+#Setup.timeManager.timeStep = Second(1)
+
 # Run the model 
 while !isRinging(clock.alarms[1])
 
-    println(clock.currTime)
-    
     advance!(clock)
     
     ocn_timestep(Prog, Diag, Tend, Setup)
 
 end 
+
+m = Setup.mesh 
+
+ds = NCDataset("./test.nc","c")
+
+# Define the dimension "lon" and "lat" with the size 100 and 110 resp.
+defDim(ds,"Time",1)
+defDim(ds,"nCells",m.nCells)
+defDim(ds,"nEdges",m.nEdges)
+defDim(ds,"nVertices",m.nVertices)
+defDim(ds,"nVertLevels",m.nVertLevels)
+defDim(ds,"maxEdges",m.maxEdges)
+defDim(ds,"TWO",m.TWO)
+
+# Define the coordinate variables 
+xCell = defVar(ds,"xCell",Float64,("nCells",))
+yCell = defVar(ds,"yCell",Float64,("nCells",))
+xEdge = defVar(ds,"xEdge",Float64,("nEdges",))
+yEdge = defVar(ds,"yEdge",Float64,("nEdges",))
+xVertex = defVar(ds,"xVertex",Float64,("nVertices",))
+yVertex = defVar(ds,"yVertex",Float64,("nVertices",))
+
+# Define the mesh metric variables 
+areaCell = defVar(ds,"areaCell",Float64,("nCells",))
+areaTriangle = defVar(ds,"areaTriangle",Float64,("nVertices",))
+
+# Define the mesh connectivity variables 
+nEdgesOnCell = defVar(ds,"nEdgesOnCell",Int32,("nCells",))
+nEdgesOnEdge = defVar(ds,"nEdgesOnEdge",Int32,("nEdges",))
+verticesOnCell = defVar(ds,"verticesOnCell",Int32,("maxEdges","nCells"))
+verticesOnEdge = defVar(ds,"verticesOnEdge",Int32,("TWO","nEdges"))
+
+# Define the data variables 
+layerThickness = defVar(ds,"layerThickness",Float64,("Time","nCells","nVertLevels"))
+normalVelocity = defVar(ds,"normalVelocity",Float64,("Time","nEdges","nVertLevels"))
+
+# dump the variables into the dataset. 
+xCell[:] = m.xCell
+yCell[:] = m.yCell
+xEdge[:] = m.xEdge
+yEdge[:] = m.yEdge
+xVertex[:] = m.xVertex
+yVertex[:] = m.yVertex
+
+areaCell[:] = m.areaCell
+areaTriangle[:] = m.areaTriangle
+
+nEdgesOnCell[:] = m.nEdgesOnCell
+nEdgesOnEdge[:] = m.nEdgesOnEdge
+verticesOnCell[:,:] = m.verticesOnCell
+verticesOnEdge[:,:] = m.verticesOnEdge
+
+layerThickness[:,:,:] = Prog.layerThickness
+normalVelocity[:,:,:] = Prog.normalVelocity
+
+close(ds)
--- src/forward/time_integration.jl
+++ src/forward/time_integration.jl
@@ -8,6 +8,8 @@ function ocn_timestep(Prog::PrognosticVars,
     
     # convert the timestep to seconds 
     dt = Dates.value(Second(Clock.timeStep))
+    
+    println(dt)
 
     # compute the diagnostics
     diagnostic_compute!(Mesh, Diag, Prog)
@@ -21,17 +23,10 @@ function ocn_timestep(Prog::PrognosticVars,
     # unpack the state and tendency variable arrays 
     @unpack normalVelocity, layerThickness = Prog
     @unpack tendNormalVelocity, tendLayerThickness = Tend 
-    
-    # swap the time levels 
-    normalVelocity[:,:,2] = normalVelocity[:,:,1]
-    layerThickness[:,:,2] = layerThickness[:,:,1]
 
     # update the state variables by the tendencies 
-    normalVelocity[:,:,2] .+= dt .* tendNormalVelocity 
-    layerThickness[:,:,2] .+= dt .* tendLayerThickness 
-
-    normalVelocity[:,:,1] = normalVelocity[:,:,2]
-    layerThickness[:,:,1] = layerThickness[:,:,2]
+    normalVelocity[:,:,1] .+= dt .* tendNormalVelocity 
+    layerThickness[:,:,1] .+= dt .* tendLayerThickness 
 
     # pack the updated state varibales in the Prognostic structure
     @pack! Prog = normalVelocity, layerThickness 
