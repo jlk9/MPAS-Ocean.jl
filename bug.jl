975934ce2da2130988e1e9add028f1a1e1b59fcf
jlk9 <josek97@utexas.edu>
2024-06-25 09:45:00 -0500

    Isolating issue to Enzyme permuting a struct field it shouldn't

--- bug.jl
+++ bug.jl
@@ -54,7 +54,7 @@ function gradient_normSq(grad, mesh::HorzMesh; backend=KA.CPU())
 
     KA.synchronize(backend)
 
-    #@show grad
+    @show grad
 
     normSq = 0.0
     for i = 1:nEdges
@@ -104,36 +104,5 @@ d_normSq = autodiff(Enzyme.Reverse,
 @show isequal(mesh.Edges.dcEdge, old_mesh.Edges.dcEdge)
 @show isequal(mesh.Edges.angleEdge, old_mesh.Edges.angleEdge)
 
-mesh.Edges.dcEdge[:] = old_mesh.Edges.dcEdge[:]
-
-# For comparison, let's compute the derivative by hand for a given scalar entry:
-gradNumP = KA.zeros(backend, Float64, (1, mesh.Edges.nEdges))
-for i = 1:mesh.Edges.nEdges
-    gradNumP[1,i] = gradNumP[1,i] + i
-end
-gradNumM = deepcopy(gradNumP)
-
-ϵ = 0.1
-k = 1837
-
-gradNumP[k] = gradNumP[k] + abs(gradNumP[k]) * ϵ
-gradNumM[k] = gradNumM[k] - abs(gradNumM[k]) * ϵ
-gradNumPk = gradNumP[k]
-gradNumMk = gradNumM[k]
-
-normSqP = gradient_normSq(gradNumP, mesh)
-normSqM = gradient_normSq(gradNumM, mesh)
-
-@show normSqP
-@show normSqM
-@show gradNumPk
-@show gradNumMk
-
-dnorm_dgrad_fd = (normSqP - normSqM) / (gradNumPk - gradNumMk)
-dnorm_dgrad    = d_gradNum[k]
-
-@info """ (gradients)\n
-For edge global index $k
-Enzyme computed $dnorm_dgrad
-Finite differences computed $dnorm_dgrad_fd
-"""
+@show old_mesh.Edges.dcEdge
+@show mesh.Edges.dcEdge
