69d1cdac4fee8af0d66eacad4b792caadc2a3bc2
Joseph Kump <josek97@utexas.edu>
2024-07-05 15:52:48 -0500

    Updates

--- test/ocn/test_Operators.jl
+++ test/ocn/test_Operators.jl
@@ -17,8 +17,8 @@ mesh_fn  = "MokaMesh.nc"
 
 Downloads.download(mesh_url, mesh_fn)
 
-backend = KA.CPU()
-#backend = CUDABackend();
+#backend = KA.CPU()
+backend = CUDABackend();
 
 # Read in the purely horizontal doubly periodic testing mesh
 HorzMesh = ReadHorzMesh(mesh_fn; backend=backend)
